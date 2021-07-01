5f6aeb97503cc1ec0c027192c2af31059d6fd4ca
Don Colombege <don.colombege@gmail.com>
2021-07-01 22:32:11 +0530

    Basic Player Movement and Enemy Rotation
    Basic Player Movement and Enemy Rotation

---
+++ Assets/Scripts/Enemy/Enemy_Movement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Movement : MonoBehaviour
+{
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x > player.position.x)
+        {
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+    }
+}
